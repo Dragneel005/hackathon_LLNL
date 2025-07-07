204788ed30bdcd854f7e19200e907d1bf89f7bd3
Islas <islas1@llnl.gov>
2025-07-07 13:07:46 -0700

    first

--- sujay.py
+++ sujay.py
@@ -6,6 +6,19 @@ while num_var > 0:
     var_eq = []
     inte = input("Enter a letter")
     var_eq.append
-    num_var
-print("Enter equation")
+    num_var = num_var - 1
+
+print("How many equations you have?")
+num_eq = int(input())
+while num_eq > 0:
+    print("Enter equation using the variables that you just created")
+    print(var_eq)
+
+    equation = input
+    num_eq = num_eq - 1
+
+
+
+
+
 
