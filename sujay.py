a7db86be18e15622a6ef7c70032da6ac0d29c42d
Kanakamedala <kanakamedala1@llnl.gov>
2025-07-07 11:19:42 -0700

    test1

---
+++ sujay.py
@@ -0,0 +1,10 @@
+print(" Something important sounding")
+print("Set variables how many do you want?")
+num_var = int(input())
+while num_var > 0:
+    print("Enter variable")
+    var_eq = []
+    inte = input("Enter a letter")
+    var_eq.append
+    num_var
+print("Enter equation")
